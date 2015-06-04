878cf36e4fc75a79363f44cea64cfcad969b4035
domacoski <domacoski.thia>
2015-06-04 01:20:33 -0300

    initial

---
+++ src/br/com/Threads.java
@@ -0,0 +1,45 @@
+package br.com;
+
+public class Threads {
+	
+	
+	public static void main(String[] args) {
+		
+		int index = 0; 
+		while(index != 10){
+			index++;
+			final Thread thread = new Thread(new Threads.Tarefa(index));
+			thread.start();
+			
+		}
+		
+	}
+	
+	
+	
+	public static class Tarefa implements Runnable{
+
+		final Integer tarefa;
+		public Tarefa(Integer tarefa) {
+			this.tarefa = tarefa;
+		}
+		
+		@Override
+		public void run() {
+			int index = 0;
+			while(index != 10){
+				
+				index++;
+				try {
+					Thread.sleep(1000L);
+					System.out.println("tarefa: "+this.tarefa+" #"+index);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+	}
+
+}
